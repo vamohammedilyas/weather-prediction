371c6e2b32eb4406ab1e5d19abfe0b8468f70ba8
Mohammed Ilyas <158069780+vamohammedilyas@users.noreply.github.com>
2025-10-13 22:28:51 +0530

    Changed the inputs in script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ async function getWeather() {
   const city = document.getElementById("cityInput").value.trim();
 
   if (!city) {
-    alert("Please enter a city name"); 
+    alert("Please Enter a City Name"); 
     return; 
   }
 
