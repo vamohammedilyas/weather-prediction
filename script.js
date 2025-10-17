7617886e07e3cc7358ed44c9680dc983e9b2e08e
Mohammed Ilyas <158069780+vamohammedilyas@users.noreply.github.com>
2025-10-17 17:01:07 +0530

    Fix alert message for missing city input

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ async function getWeather() {
     alert("Please Enter a City Name");  
     return; 
   } 
-
+ 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${apiKey}&units=metric`;
  
   try {
